8fbeddb03e054b83e67632e55d71b26002a513dd
LeeGyumin <dbsrall59@gmail.com>
2022-07-11 12:23:58 +0900

    a

--- lolgetinfo.py
+++ lolgetinfo.py
@@ -53,24 +53,25 @@ LOCATION = 'C:' + os.environ['HOMEPATH'] + '\\instantclient_21_6'
 os.environ['PATH'] = LOCATION + ';' + os.environ['PATH']
 os.environ['NLS_LANG'] = 'KOREAN_KOREA.KO16MSWIN949'
 
-#테이블 2500 최대 75000 30
-tablesize = 2500
+#테이블 3900 최대 117000 30
+tablesize = 3900
 
-conn = db.connect('lolpredict','korhrd','163.152.224.168:1521')
+conn = db.connect('lolpredict', 'korhrd', '127.0.0.1:1521')
 cursor = conn.cursor()
 
 sql = 'select * from userinfo where isComplete = 0'
 cursor.execute(sql)
 userList = cursor.fetchall()
 
-sql = 'select * from matchinfo order by matchid'
+sql = 'select matchid from matchinfo order by matchid'
 cursor.execute(sql)
 matchList = cursor.fetchall()
 
 requestHeader = {
-
+    
 }
 
+maxlength = 0
 while(len(userList)):
     i0 = random.randint(0, len(userList) - 1)
     
@@ -81,15 +82,17 @@ while(len(userList)):
         puuid = requests.get(nameUrl, headers=requestHeader)
         nameCode = puuid.status_code
         time.sleep(2)
-        
-        print(nameCode)
+        print(puuid.status_code)
+            
         if(nameCode == 404):
             isnotFound = True
             break
     
     if(isnotFound == True):
+        sql = "update userinfo SET iscomplete = 1 where username = '{}'".format(userList[i0][0])
+        cursor.execute(sql)
+        conn.commit()
         continue
-    
     
     puuidUrl = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid.json()['puuid'] + "/ids?start=0&count=100"
     puuidCode = 0
@@ -97,7 +100,7 @@ while(len(userList)):
         puuidResult = requests.get(puuidUrl, headers=requestHeader)
         puuidCode = puuidResult.status_code
         time.sleep(2)
-        print(puuidCode)
+        print(puuidResult.status_code)
         
     for matchid in puuidResult.json():
         searchMatch = binarySearchtuple(matchid, matchList, 0)
@@ -110,7 +113,7 @@ while(len(userList)):
             matchResult = requests.get(matchUrl, headers=requestHeader)
             matchCode = matchResult.status_code
             time.sleep(2)
-            print(matchCode)
+            print(matchResult.status_code)
             
         for user in matchResult.json()['info']['participants']:
             searchUser = binarySearchtuple(user['summonerName'], userList, 0)
@@ -143,7 +146,7 @@ while(len(userList)):
             cursor.execute(sql)
             conn.commit()
         except:
-            break
+            continue
         
     sql = "update userinfo SET iscomplete = 1 where username = '{}'".format(userList[i0][0])
     cursor.execute(sql)
@@ -153,6 +156,6 @@ while(len(userList)):
     cursor.execute(sql)
     userList = cursor.fetchall()
     
-    sql = 'select * from matchinfo order by matchid'
+    sql = 'select matchid from matchinfo order by matchid'
     cursor.execute(sql)
     matchList = cursor.fetchall()
